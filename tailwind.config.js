a9473185f66aae34b59a5e025429e4550bda5992
ndkhoa1195 <ndkhoa1195@gmail.com>
2024-07-24 13:46:03 +0700

    finish project

--- tailwind.config.js
+++ tailwind.config.js
@@ -10,6 +10,14 @@ module.exports = {
         'desaturated-blue': 'hsl(224, 23%, 55%)',
         'dark-blue': 'hsl(223, 47%, 23%)',
       },
+      backgroundImage: {
+        desktop: "url('../images/pattern-background-desktop.svg')",
+        mobile: "url('../images/pattern-background-mobile.svg')",
+      },
+      boxShadow: {
+        box: '0px 40px 40px -20px rgba(13, 48, 189, 0.15)',
+        button: '0px 20px 20px 0px rgba(56, 42, 225, 0.19)',
+      },
     },
   },
   plugins: [],
